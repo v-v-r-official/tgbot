4af0c49b572c4b3fe24870a5c927cd7b0c843041
Good Boy <78008481+v-v-r-official@users.noreply.github.com>
2021-02-21 18:55:41 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,6 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
+
 Hello,
 This bot is exclusively made for @VKprojects
 
