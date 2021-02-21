ec0587082620d5014657b3d19aff72e650251e08
Good Boy <78008481+v-v-r-official@users.noreply.github.com>
2021-02-21 20:28:26 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -139,7 +139,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="Group", url="https://t.me/VKP_BOTS"), InlineKeyboardButton(text="Channel", url="https://t.me/VKPROJECTS") ]]))
+                    [[InlineKeyboardButton(text="Group", url="https://t.me/vkp_bots"), InlineKeyboardButton(text="Channel", url="https://t.me/vkprojects") ]]))
 
     else:
         update.effective_message.reply_text("HelLO")
